c201bec55834d100f17b8a44e524753c5a5ae38a
Max Strange <max.strange@synapse.com>
2016-10-09 22:20:01 -0700

    got the reset function of the preprocessor working... I think

--- preprocessing/mover.py
+++ preprocessing/mover.py
@@ -2,6 +2,8 @@
 Module for moving files around for the preprocessor.
 """
 
+import os
+import shutil
 import preprocessing.prep_global as prep_global
 import chef_global.debug as debug
 import chef_global.config as config
@@ -18,5 +20,24 @@ def _copy_master_to_data_location():
                         "config.MASTER_DATA_DIRECTORY")
     debug.assert_value_is_set(config.DATA_DIRECTORY,
                         "config.DATA_DIRECTORY")
-    print("Moving data files from " + str(config.MASTER_DATA_DIRECTORY) +
+
+    print("Deleting old data files...")
+    prep_global._apply_func_to_each_data_file(os.remove)
+
+    print("Copying data files from " + str(config.MASTER_DATA_DIRECTORY) +
             " to " + str(config.DATA_DIRECTORY))
+    # Collect files to copy
+    list_of_file_names = [os.path.join(config.MASTER_DATA_DIRECTORY, file_name) for
+                          file_name in os.listdir(config.MASTER_DATA_DIRECTORY)]
+
+    for f in list_of_file_names:
+        f_name = os.path.split(f)[-1]
+        debug.debug_print("Copying " + f_name + " to new directory...")
+        shutil.copyfile(f, os.path.join(config.DATA_DIRECTORY, f_name))
+
+
+
+
+
+
+
--- preprocessing/prep_global.py
+++ preprocessing/prep_global.py
@@ -56,7 +56,7 @@ def _apply_func_to_each_data_file(func):
 
     # Collect each file in the directory
     assert_value_is_set(config.DATA_DIRECTORY, "config.DATA_DIRECTORY")
-    list_of_file_names = [file_name for
+    list_of_file_names = [os.path.join(config.DATA_DIRECTORY, file_name) for
                             file_name in os.listdir(config.DATA_DIRECTORY)]
 
     for f in list_of_file_names:
