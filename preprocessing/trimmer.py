e049b7ed0d7c33dc3f7d8474f0ceaa9be797578d
Max Strange <max.strange@synapse.com>
2016-10-10 09:13:00 -0700

    working on preprocessing pipeline

--- preprocessing/trimmer.py
+++ preprocessing/trimmer.py
@@ -14,6 +14,11 @@ def __trim_non_recipe(cookbook_file_path):
     @return: void
     """
     #TODO
+    #Read the file until we find <recipe>, now record the lines into memory
+    #until we find </recipe>, at which point we should dump those lines
+    #into a tmp file
+    #Repeat until end of file
+    #Delete contents of old file and replace with tmp's contents. Then delete tmp
     debug.debug_print("Attempting to trim " + str(cookbook_file_path))
 
 def _trim_all_files_to_recipes():
